7bb4784de72942911defbc1bc6d76c9eea8d24ed
Saharsh2k5 <saharshburra@gmail.com>
2026-05-21 19:51:54 -0400

    revised compiler commands

---
+++ compiler_revise/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+# define GREETING "Hello from Hitec City"
+int add(int a,int b);
+int main(){
+    int result=add(3,4);
+    printf("Results in : %d\n",result);
+    return 0;
+}
